e04817a274bae08e1c74fc053aa847a9fdbfd38e
Konstantin Zamaraev <zamaraevk@gmail.com>
2016-09-17 18:58:03 -0400

    index.html change

--- js/MyTitle.js
+++ js/MyTitle.js
@@ -1,3 +1,21 @@
+/* ORIGINAL
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+        h1({style: {color: this.props.color}}, this.props.title)
+      )
+    )
+  }
+})
+*/
+
+module.exports = MyTitle
+
+var React = require('react')
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
