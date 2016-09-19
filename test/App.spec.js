61354f212ffc56c4c836ac1dddc9e5abd28b0bc0
Konstantin Zamaraev <zamaraevk@gmail.com>
2016-09-18 23:40:57 -0400

    2 react unit tests

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,9 +1,20 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
+const React = require('react')
+const Search = require('../js/Search')
+const ShowCard = require('../js/ShowCard')
+const { shallow } = require('enzyme')
+const { shows } = require('../public/data')
 
 describe('<Search />', () => {
-  it('should pass', () => {
-    expect(1 + 1 === 2).to.be.true
+  it('should render the brand', () => {
+    // shallow - will render parent component but not its childrens( much faster! )
+    const wrapper = shallow(<Search />)
+    expect(wrapper.contains(<h1 className="brand">svideo</h1>)).to.be.true
+  })
+  it('it should render as many tests as there are data for', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(ShowCard).length).to.equal(shows.length)
   })
 })
